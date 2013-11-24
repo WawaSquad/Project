559c337ddf70bdabb3ea56f879686e54695d08d7
daisyyifeng <daisyyifeng91@gmail.com>
2013-11-24 09:19:51 -0500

    rate

--- routes/add_rating.js
+++ routes/add_rating.js
@@ -13,25 +13,62 @@ function insert_rating(res,photoID_str,score_str) {
 	    if ( err ) {
 	    	console.log(err);
 	    } else {
-	    	
-	    	var query="INSERT INTO  Rating VALUES ( '"+photoID+"', '"+userID+"', '"+score+"')";
-	    	console.log(query);
-		  	connection.execute(query, 
+	    	var initquery="SELECT * FROM Rating WHERE photoID = "+photoID+" AND userID = '"+userID+"' ";
+	    	console.log(initquery);
+	    	var query;
+	    	connection.execute(initquery, 
 		  			   [], 
 		  			   function(err, results) {
 		  	    if ( err ) {
 		  	    	console.log(err);
+		  	    	window.alert("Rating editing failed!");
 		  	    } else {
-		  	    	connection.close(); // done with the connection
+		  	    	if(results.length==0){
+		  	    	   query="INSERT INTO  Rating VALUES ( "+photoID+", '"+userID+"', "+score+")";
+		  		       connection.execute(query, 
+		  			  	 [], 
+		  			     function(err, results) {
+		  			  	    if ( err ) {
+		  			  	    	console.log(err);
+		  			  	    	output_result(res,"Failed to insert rating for ",photoID);
+		  			  	    } else {
+		  			  	    	output_result(res,"Successfully added rating for ",photoID);
+		  			  	    	connection.close(); // done with the connection
+		  			  	    }
+		  			
+		  			  	}); 
+		  	    	}
+		  	    	
+		  	    	else{
+		  	    		query="UPDATE Rating SET score="+ score+" WHERE photoID = "+photoID+" AND userID = '"+userID+"' ";
+		  		    	
+		  			  	connection.execute(query, 
+		  			  			   [], 
+		  			  			   function(err, results) {
+		  			  	    if ( err ) {
+		  			  	    	output_result(res,"Failed to update rating for ", photoID);
+		  			  	    	console.log(err);
+		  			  	    } else {
+		  			  	    	output_result(res,"Successfully updated rating for Photo "+photoID);
+		  			  	    	connection.close(); // done with the connection
+		  			  	    }
+		  			
+		  			  	}); 
+		  	    	}
 		  	    }
 		
 		  	}); // end connection.execute
+	    
 	    }
 	  }); // end oracle.connect
 	}
 
 exports.add_rating = function(req, res){
-	insert_rating(res,req.query.photoID,req.query.score);
+	insert_rating(res,req.query.currentphotoID,req.query.score);
 };
 
-
+function output_result(res,msg,photoID){
+	res.render('add_rating_result',
+			{msg: msg,
+		    photoID: photoID});
+}
