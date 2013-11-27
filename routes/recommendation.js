7ba2c93c4b70981d35e53b4e366449d29cd3e4fb
daisyyifeng <daisyyifeng91@gmail.com>
2013-11-27 12:12:37 -0500

    update recommendation.js

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -16,16 +16,16 @@ function query_db_recommendation_method(res) {
 	    	var subquery1="(SELECT Object.id, object.url FROM Object, Tags "+
 	    	"WHERE Object.type='photo' AND Tags.id=Object.id AND Object.source=Tags.source"+
 	    	"AND Tags.tag IN (SELECT Tags.tag FROM Pin, FriendShip, Tags "+
-	    	"WHERE Pin.login=FriendShip.friendID AND Pin.id=Tags.id AND Pin.sourceId= Tags.source "+
-	    	"AND FriendShip.userID='"+userID+"')";
+	    	"WHERE Pin.login=FriendShip.friendID AND Pin.objectId=Tags.id AND Pin.sourceId= Tags.source "+
+	    	"AND FriendShip.login='"+userID+"'))";
 	    	
 	    	var subquery2="(SELECT Object.id, object.url FROM Object, Tags "+
 	    	"WHERE Object.type='photo' AND Tags.id=Object.id AND Object.source=Tags.source"+
 	    	"AND Tags.tag IN (SELECT Interests.interest FROM Users, Interests  "+
-	    	"WHERE Users.login=Interests.login AND Users.login='"+userID+"')";
+	    	"WHERE Users.login=Interests.login AND Users.login='"+userID+"'))";
 	    	
 	    	var subquery3="(SELECT Object.id, object.url FROM Users, Pin, Object  "+
-	    	"WHERE Object.type='photo' AND Tags.id=Object.id AND Object.source=Tags.source"+
+	    	"WHERE Object.type='photo' AND Users.login=PIn.login"+
 	    	"AND Pin.objectId=Object.id AND Pin.sourceId=Object.source AND Users.login='"+userID+"')";
 	    	
 	    	var query="SELECT * FROM (SELECT * FROM ("+subquery1+" UNION "+subquery2+" MINUS "+subquery3+") ORDER BY dbms_random.value) WHERE ROWNUM<=10"
