34c4c5619ffb756474b5986da5c73b321406830a
daisyyifeng <daisyyifeng91@gmail.com>
2013-11-30 18:21:53 -0500

    can add a new board when pin

--- routes/addBoard.js
+++ routes/addBoard.js
@@ -6,9 +6,9 @@ var connectData = {
 	  "database": "PENNTR" };
 	var oracle =  require("oracle");
 
-	function query_db(res,boardName) {
+	function query_db(res,boardName,currentObjID,currentSrcID) {
 		
-		
+		  console.log(currentObjID+" "+currentSrcID);
 		  oracle.connect(connectData, function(err, connection) {
 		    if ( err ) {
 		    	console.log(err);
@@ -28,13 +28,13 @@ var connectData = {
 						if (results[0] == null )
 							{
 							console.log("No Board Found")
-							query_db2(res,boardName);
+							query_db2(res,boardName,currentObjID,currentSrcID);
 							}
 						else
 							{
 							fail = true;
 							console.log("Error: Board Found");
-                            query_db3(res,success=false,fail);
+                            query_db3(res,success=false,fail,currentObjID,currentSrcID);
 							
 							}
 						
@@ -46,7 +46,7 @@ var connectData = {
 		  }); // end oracle.connect
 		}
 	
-	function query_db2(res,boardName) {
+	function query_db2(res,boardName,currentObjID,currentSrcID) {
 		
 		 oracle.connect(connectData, function(err, connection) {
 			    if ( err ) {
@@ -70,7 +70,8 @@ var connectData = {
 				  	    console.log("Sucessfully Inserted Board");
 				  	    connection.close();
 				  	    success = true;
-				  	    query_db3(res,success, fail=false);
+				  	  console.log(currentObjID+" "+currentSrcID);
+				  	    query_db3(res,success, fail=false,currentObjID,currentSrcID);
 				  	    }
 				  	}); // end connection.execute
 				    }
@@ -78,6 +79,34 @@ var connectData = {
 				}
 	
 	
+	function query_db3(res, success, fail, currentObjID, currentSrcID) {
+		  oracle.connect(connectData, function(err, connection) {
+		    if ( err ) {
+		    	console.log(err);
+		    } else {
+		    	var query = "SELECT name, login FROM Board WHERE " +
+		    	"login = '"+userID+"'";
+			  	
+		    	connection.execute(query, 
+			  			   [], 
+			  			   function(err, results) {
+			  	    if ( err ) {
+			  	    	console.log(err);
+			  	    } else {
+			  	    	connection.close(); // done with the connection
+			  	    	console.log(results);
+			  	    	if(fail)
+			  	    		resultFail(res,results,fail, currentObjID, currentSrcID);
+			  	    	if(success)
+			  	    		resultSuccess(res,results,success, currentObjID, currentSrcID);
+			  	    }
+			
+			  	}); // end connection.execute
+		    }
+		  }); // end oracle.connect
+		}
+	
+	/*
 	function query_db3(res,success,fail) {
 		  oracle.connect(connectData, function(err, connection) {
 		    if ( err ) {
@@ -105,17 +134,21 @@ var connectData = {
 		    }
 		  }); // end oracle.connect
 		}
+	*/
 	
 	exports.addBoard = function(req, res){
-		query_db(res,req.query.NewBoardName);
+		query_db(res,req.query.NewBoardName,req.query.currentObjID,req.query.currentSrcID);
 	};
 	
 	
-	function resultSuccess(res,results,success) {
-		res.render('board',
+	function resultSuccess(res,results,success,currentObjID, currentSrcID) {
+		console.log("render ID success:"+ currentObjID+" "+currentSrcID);
+		res.render('pin_it',
 			  {
 		      success: success,
 		      fail: fail =false,
+		      currentObjID: currentObjID,
+			  currentSrcID: currentSrcID,
 		      results: results
 
 				}	
@@ -123,11 +156,14 @@ var connectData = {
 		  );
 	}
 
-		function resultFail(res,results, fail) {
-			res.render('board',
+		function resultFail(res,results, fail,currentObjID, currentSrcID) {
+			console.log("render ID fail:"+ currentObjID+" "+currentSrcID);
+			res.render('pin_it',
 				  {
 			      fail: fail,
 		          success: success=false,
+		          currentObjID: currentObjID,
+				  currentSrcID: currentSrcID,
 		          results: results
 					}	
 			
