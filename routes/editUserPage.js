c49801cb051766db8df53a100e62efa21961a78c
daisyyifeng <daisyyifeng91@gmail.com>
2013-12-02 18:36:51 -0500

    changed bootstrap css

--- routes/editUserPage.js
+++ routes/editUserPage.js
@@ -4,9 +4,14 @@
 exports.editUserPage = function(req, res){
 	//global.userID=req.body.userID;
 	//show_user_info(res,userID);
-	show_user_info(res,userID);
+	loadPage(res,userID);
 
 };
+
+
+
+
+//var loadUserpage = "./userPage.js";
 exports.editInfo = function(req, res){
 	//global.userID=req.body.userID;
 	editInformation(res,userID,req.body.email,req.body.givenname,req.body.affiliation,req.body.surname);
@@ -24,7 +29,7 @@ var connectData = {
   "database": "PENNTR" };
 var oracle =  require("oracle");
 
-function show_user_info(res,userID) {
+function loadPage(res,userID) {
 	var intPageNum;
 	  oracle.connect(connectData, function(err, connection) {
 	    if ( err ) {
@@ -77,7 +82,8 @@ function 	editInformation(res,userID,email,givenname,affiliation,surname) {
 		  	    } else {
 		  	    	console.log("Query Executed")
 		  	    	connection.close(); // done with the connection
-		  	    	renderUser(res,userID);
+		  	    	console.log("loading user page");
+		  	  	query_db_recommendation(res,userID); 
 		  	    }
 		
 		  	}); // end connection.execute
@@ -85,13 +91,41 @@ function 	editInformation(res,userID,email,givenname,affiliation,surname) {
 	  }); // end oracle.connect
 }
 
-function renderUser(res,userID) {
+//CODE TO LOAD USERPAGE
+function show_user_info(res,userID,recommendationResults,msg) {
+	var intPageNum;
 	  oracle.connect(connectData, function(err, connection) {
 	    if ( err ) {
 	    	console.log(err);
 	    } else {
 
 	    	var query="SELECT *  FROM Users WHERE Users.login = '" +  userID + 
+	  			"'  ";
+	    	
+		  	connection.execute(query, 
+		  			   [], 
+		  			   function(err, results) {
+		  	    if ( err ) {
+		  	    	console.log(err);
+		  	    } else {
+		  	    	connection.close(); // done with the connection
+		  	    	showInterest(res,results,recommendationResults,msg);
+		  	    	
+		  	    }
+		
+		  	}); // end connection.execute
+	    }
+	  }); // end oracle.connect
+	}
+
+
+function showInterest(res,userInfo,recommendationResults,msg) {
+	var intPageNum;
+	  oracle.connect(connectData, function(err, connection) {
+	    if ( err ) {
+	    	console.log(err);
+	    } else {
+	    	var query="SELECT Interest  FROM Interests WHERE Interests.login = '" +  userID + 
 	  			"' ";
 	    	
 		  	connection.execute(query, 
@@ -101,19 +135,129 @@ function renderUser(res,userID) {
 		  	    	console.log(err);
 		  	    } else {
 		  	    	connection.close(); // done with the connection
-		  	    	loadPage(res,results);
+		  	   
+		  	    	renderUserpage(res, userInfo,recommendationResults,msg,results);
 		  	    }
 		
 		  	}); // end connection.execute
 	    }
 	  }); // end oracle.connect
 	}
-function loadPage(res,results) {
-	res.render('userPage',
-		  {
-		  results: results
 
-			}	
-	
+function renderUserpage(res, results,recommendationResults,msg,interest) {
+	res.render('userPage',
+		   {
+			results: results,
+			recommendationResults: recommendationResults,
+			msg: msg,
+			interest: interest}
 	  );
 }
+
+
+
+function query_db_recommendation(res,userID) {
+	  oracle.connect(connectData, function(err, connection) {
+	    if ( err ) {
+	    	console.log(err);
+	    } else {
+	    	var subquery1="(SELECT Object.id, object.url, object.source FROM Object, Tags "+
+	    	"WHERE Object.type='photo' AND Tags.id=Object.id AND Object.source=Tags.source "+
+	    	"AND Tags.tag IN (SELECT Tags.tag FROM Pin, FriendShip, Tags "+
+	    	"WHERE Pin.login=FriendShip.friendID AND Pin.objectId=Tags.id AND Pin.sourceId= Tags.source "+
+	    	"AND FriendShip.login='"+userID+"'))";
+	    	console.log(subquery1);
+	    	
+	    	var subquery2="(SELECT Object.id, object.url, object.source FROM Object, Tags "+
+	    	"WHERE Object.type='photo' AND Tags.id=Object.id AND Object.source=Tags.source "+
+	    	"AND Tags.tag IN (SELECT Interests.interest FROM Users, Interests  "+
+	    	"WHERE Users.login=Interests.login AND Users.login='"+userID+"'))";
+	    	console.log(subquery2);
+	    	
+	    	var subquery3="(SELECT Object.id, object.url, object.source FROM Object, Tags "+
+	    	"WHERE Object.type='photo' AND Tags.id=Object.id AND Object.source=Tags.source "+
+	    	"AND Tags.tag IN (SELECT Tags.tag FROM Pin, Tags "+
+	    	"WHERE  Pin.objectId=Tags.id AND Pin.sourceId= Tags.source "+
+	    	"AND Pin.login='"+userID+"'))";
+	    	console.log(subquery3);
+	    	
+	    	var subquery4="(SELECT Object.id, object.url, object.source FROM Object, (select * from (select "+
+	    	"Rating.objectId, Rating.sourceId from Rating where Rating.login='"+userID+"' "+
+	    	"order by rating DESC) where rownum<=10) R where Object.id=R.objectId and Object.source=R.sourceId)";
+	    	console.log(subquery4);
+	    	
+	    	var subquery5="(SELECT Object.id, object.url, object.source FROM Users, Pin, Object  "+
+	    	"WHERE Object.type='photo' AND Users.login=Pin.login "+
+	    	"AND Pin.objectId=Object.id AND Pin.sourceId=Object.source AND Users.login='"+userID+"')";
+	    	console.log(subquery5);
+	    	
+	    	var query="SELECT * FROM (SELECT * FROM ("+subquery1+" UNION "+subquery2+" UNION "+subquery3+" UNION "+subquery4+" MINUS "+subquery5+") ORDER BY dbms_random.value) WHERE ROWNUM<=5"
+	    	console.log(query);
+	    	connection.execute(query, 
+		  			   [], 
+		  			   function(err, results) {
+		  	    if ( err ) {
+		  	    	console.log(err);
+		  	    } else {
+		  	    	connection.close();
+		  	    	if(results.length==0){
+		  	    		query_db_recommendation2(res,userID);
+		  	    	}
+		  	    	else{
+		  	    		var msg="Go through these recommendations and see if you like!";
+		  	    		show_user_info(res,userID,results,msg);
+		  	    	}
+		  	    	}
+		  		
+		
+		  	}); // end connection.execute
+	    } 
+	  }); // end oracle.connect
+	}
+
+
+function query_db_recommendation2(res,userID) {
+	  oracle.connect(connectData, function(err, connection) {
+	    if ( err ) {
+	    	console.log(err);
+	    } else {
+	    	var subquery1="(SELECT Object.id, object.url, object.source FROM Object, (select * from (select "+
+	    	"Pin.objectId, Pin.sourceId from Pin group by Pin.objectId, Pin.sourceId "+
+	    	"order by count(*) DESC) where rownum<=10) P where Object.id=P.objectId and Object.source=P.sourceId)"
+	    	console.log(subquery1);
+	    	
+	    	var subquery2="(SELECT Object.id, object.url, object.source FROM Object, (select * from (select "+
+	    	"Rating.objectId, Rating.sourceId from Rating GROUP BY Rating.objectId, Rating.sourceId  "+
+	    	"order by avg(rating) DESC) where rownum<=10) R where Object.id=R.objectId and Object.source=R.sourceId AND Object.type='photo')"
+	    	console.log(subquery2);
+	    	
+	    	var subquery3="(SELECT Object.id, object.url, object.source FROM Users, Pin, Object  "+
+	    	"WHERE Object.type='photo' AND Users.login=Pin.login "+
+	    	"AND Pin.objectId=Object.id AND Pin.sourceId=Object.source AND Users.login='"+userID+"')";
+	    	console.log(subquery3);
+	    	
+	    	var query="SELECT * FROM (SELECT * FROM ("+subquery1+" UNION "+subquery2+" MINUS "+subquery3+") ORDER BY dbms_random.value) WHERE ROWNUM<=5"
+	    	console.log(query);
+	    	connection.execute(query, 
+		  			   [], 
+		  			   function(err, results) {
+		  	    if ( err ) {
+		  	    	console.log(err);
+		  	    } else {
+		  	    	connection.close();
+		  	    	if(results.length==0){
+		  	    		var msg="Sorry, we donot have recommendations for you now, please be more active!";
+		  	    		show_user_info(res,userID,results,msg);
+		  	    	}
+		  	    	else{
+		  	    		var msg="Go through these recommendations and see if you like!";
+		  	    		show_user_info(res,userID,results,msg);
+		  	    	}
+		  	    	}
+		  		
+		
+		  	}); // end connection.execute
+	    } 
+	  }); // end oracle.connect
+	}
+
