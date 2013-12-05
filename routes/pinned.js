77a387655f832ca7fe7d78e907486d0beb698882
joohong4 <joohong4@gmail.com>
2013-12-04 21:19:44 -0500

    request module

--- routes/pinned.js
+++ routes/pinned.js
@@ -21,11 +21,12 @@ function insert_pin(res,objID_str, srcID_str, boardName_str) {
 		  			 function(err, results) {
 		  			 if ( err ) {
 		  			  	    	console.log(err);
-		  			  	    	console.log(query);
+		  			  	    	console.log("error for:"+query);
 		  			  	    	console.log(objID_str);
 		  			  	    	console.log(srcID_str);
 		  			  	    	output_result(res,"You probably already haved this pinned on this board...failed to pin on ", boardName);
 		  			  	    } else {
+		  			  	    	check_pinning_time(objID, srcID);
 		  			  	    	output_result(res,"Successfully added pin to ", boardName);
 		  			  	    	connection.close(); // done with the connection
 		  	    }
@@ -43,4 +44,89 @@ function output_result(res,msg,boardName){
 	res.render('pinned',
 			{msg: msg,
 		    boardName: boardName});
+}
+
+function check_pinning_time(objID, srcID){
+	
+	oracle.connect(connectData, function(err, connection) {
+	    if ( err ) {
+	    	console.log(err);f
+	    } else {
+	    	query="SELECT count(*) NUM from Pin where objectId="+objID+" and sourceId='"+ 
+	    		srcID+"' group by objectId, sourceId";
+		  		connection.execute(query, 
+		  			 [], 
+		  			 function(err, results) {
+		  			 if ( err ) {
+		  			  	    	console.log(err);
+		  			  	    	console.log("error executing:"+query);
+		  			  	    	console.log(objID);
+		  			  	    	console.log(srcID);
+		  			  	    } else {
+		  			  	    	connection.close(); // done with the connection
+		  			  	    	var num= results[0].NUM;
+		  			  	    	console.log("number pinned before: "+num);
+		  			  	    	if(num>=4){
+		  			  	    		indicateCache(objID, srcID)
+		  			  	    		
+		  			  	    	}
+		  	    }
+				
+	  	}); // end connection.execute
+    }
+  }); // end oracle.connect
+}
+
+function indicateCache(objID, srcID){
+	oracle.connect(connectData, function(err, connection) {
+	    if ( err ) {
+	    	console.log(err);
+	    } else {
+	    	query="UPDATE Object set isCached='T' where id="+objID+" and source='"+ 
+	    		srcID+"'";
+		  		connection.execute(query, 
+		  			 [], 
+		  			 function(err, results) {
+		  			 if ( err ) {
+		  			  	    	console.log(err);
+		  			  	    	console.log("error for:"+query);
+		  			  	    } else {
+		  			  	    	connection.close(); // done with the connection
+		  			  	    	query_url(objID, srcID);
+		  			  	    	
+		  	    }
+				
+	  	}); // end connection.execute
+    }
+  }); // end oracle.connect
+
+}
+
+function query_url(objID, srcID){
+	oracle.connect(connectData, function(err, connection) {
+	    if ( err ) {
+	    	console.log(err);
+	    } else {
+	    	query="SELECT url from Object where id="+objID+" and source='"+ 
+	    		srcID+"'";
+		  		connection.execute(query, 
+		  			 [], 
+		  			 function(err, results) {
+		  			 if ( err ) {
+		  			  	    	console.log(err);
+		  			  	    	console.log(query);
+		  			  	    } else {
+		  			  	    	connection.close(); // done with the connection
+		  			  	    	var url=results[0].URL;
+		  			  	    		//insert into mongo 
+		  			  	    	console.log("URL is: "+url);
+		  			  	    	
+		  			  	    	//store to mongo below
+		  		    	
+		  	    }
+				
+	  	}); // end connection.execute
+    }
+  }); // end oracle.connect
+
 }
