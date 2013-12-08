c8fc501ee2f7d507fde23c8b8657ae719dc5ff51
daisyyifeng <daisyyifeng91@gmail.com>
2013-12-08 11:20:02 -0500

    displaying url for vedio and pdf since we have imported data. for recommendation only consider photos

--- routes/photos.js
+++ routes/photos.js
@@ -18,9 +18,9 @@ function query_db(res,boardName, nextPage) {
 			    	//"SELECT count(*) From Photo" 
 			    	//
 			    	console.log("fetching the photos....");
-			    	query="select comb.url,comb.objectId, comb.sourceId, comb.isCached, listagg(T.tag,', ') within group(order by T.tag) TAGS from "+
-			    	"(select O.url, P.objectId, P.sourceid, O.iscached from Object O, Pin P where O.type='photo' and P.sourceId=O.source and P.login ='"+userID+"' and P.board = '"+boardName+"' and P.objectId = O.id )comb "+
-			    	"left outer join Tags T On comb.objectId=T.id and comb.sourceId= T.source group by (comb.url,comb.objectid,comb.sourceid,comb.iscached)";
+			    	query="select comb.url,comb.objectId, comb.sourceId, comb.type, comb.isCached, listagg(T.tag,', ') within group(order by T.tag) TAGS from "+
+			    	"(select O.url, P.objectId, P.sourceid, O.type, O.iscached from Object O, Pin P where  P.sourceId=O.source and P.login ='"+userID+"' and P.board = '"+boardName+"' and P.objectId = O.id )comb "+
+			    	"left outer join Tags T On comb.objectId=T.id and comb.sourceId= T.source group by (comb.url,comb.objectid,comb.sourceid,comb.type, comb.iscached)";
 				  	connection.execute( query, 
 				  			   [], 
 				  			   function(err, results) {
